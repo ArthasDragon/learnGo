d4669c1aea1da14c2e48596fe0b2cd6a272eee2f
ArthasDragon <1641627793@qq.com>
2022-07-28 17:36:53 +0800

    1

--- src/learnGo/main.go
+++ src/learnGo/main.go
@@ -21,11 +21,57 @@ func main() {
 	//var err error
 
 	//conn, err := net.Dial("tcp", "www.baidu.com")
-	//conn, err1 := net.Dial("tcp", "www.baidu.com")
+	//conn, _ := net.Dial("tcp", "www.baidu.com")
 	//fmt.Println(conn)
-	//fmt.Println(err)
-	//fmt.Println(err1)
+	//
+	//a, b = b, a
+	//fmt.Println(a, b)
 
-	a, b = b, a
-	fmt.Println(a, b)
+	//var a = 100
+	//var b = 200
+	//c := sum(a, b)
+	//fmt.Printf("sum=%d", c)
+
+	//	切片
+	//var arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	//arr1 := arr[0:4]
+	//arr2 := arr[2:6]
+	//fmt.Println(len(arr1), cap(arr1))
+	//fmt.Println(len(arr2), cap(arr2))
+	//
+	//arr3 := arr2[2:4]
+	//fmt.Println(len(arr3), cap(arr3), arr3)
+
+	//s1 := make([]int, 3, 10)
+	//fmt.Printf("s1：%v len(s1)：%d cap(s1)：%d\n", s1, len(s1), cap(s1))
+	//s1 = []int{1, 2, 3}
+	//fmt.Printf("s1：%v len(s1)：%d cap(s1)：%d\n", s1, len(s1), cap(s1))
+	//
+	//for i := 0; i < len(s1); i++ {
+	//	fmt.Println(s1[i])
+	//}
+	//for i, v := range s1 {
+	//	fmt.Println(i, v)
+	//}
+	//s2 := []int{4, 5}
+	//s1 = append(s1, s2...)
+	//s3 := make([]int, 6)
+	//copy(s3, s1)
+	//fmt.Printf("s1：%v len(s1)：%d cap(s1)：%d\n", s1, len(s1), cap(s1))
+	//fmt.Println(s3)
+	//
+	////	删除切片中的元素
+	//s1 = append(s1[:1], s1[2:]...)
+	//fmt.Println(s1)
+
+	//	数组转切片
+	var s4 = [...]int{1, 2, 3}
+	fmt.Printf("s4类型：%T", s4)
+	s5 := s4[:]
+	fmt.Printf("s5类型：%T", s5)
+
+}
+
+func sum(a, b int) int {
+	return a + b
 }
